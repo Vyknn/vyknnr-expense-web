b49db8c82761dc2ca8ed24d6e4002fad2378263f
Vyknn <peerapon@outlook.jp>
2026-09-22 22:22:32 +0700

    fix: modify body limit size

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ["sharp"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
